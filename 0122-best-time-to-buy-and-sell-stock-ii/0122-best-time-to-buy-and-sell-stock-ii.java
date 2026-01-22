8cbf585ac2fec7218de03de6ed615436891ed7fb
pavanpasupuleti344 <pavanpasupuleti334@gmail.com>
2026-01-22 14:19:22 +0530

    Time: 0 ms (100%), Space: 45.7 MB (37.84%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // int minp=prices[0];
+        int pro=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i-1]<prices[i]){
+                pro+=prices[i]-prices[i-1];
+            }
+        }
+        return pro;
+    }
+}
