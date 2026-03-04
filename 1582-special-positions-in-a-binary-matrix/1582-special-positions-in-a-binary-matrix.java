c6116e337a610c3a73d68c16c4d97afc163740b1
pavanpasupuleti344 <pavanpasupuleti334@gmail.com>
2026-03-04 20:57:01 +0530

    Time: 2 ms (94.84%), Space: 47.2 MB (44.84%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,28 +1,46 @@
 class Solution {
     public int numSpecial(int[][] mat) {
         int count=0;
-        List<Integer> row=new ArrayList<>();
-        List<Integer> col=new ArrayList<>();
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
+        // List<Integer> row=new ArrayList<>();
+        // List<Integer> col=new ArrayList<>();
+        // for(int i=0;i<mat.length;i++){
+        //     for(int j=0;j<mat[i].length;j++){
+        //         if(mat[i][j]==1){
+        //             row.add(i);
+        //             col.add(j);
+        //         }
+        //     }
+        // }
+        // // for(int i=0;i<mat.length;i++){
+        // //     for(int j=0;j<mat[i].length;j++){
+        // //         if(mat[i][j]==1&&Collections.frequency(row,i)==1&&Collections.frequency(col,j)==1){
+        // //             count++;
+        // //             break;
+        // //         }
+
+        // //     }
+        // // }
+        // for(int i=0;i<row.size();i++){
+        //     if(Collections.frequency(row,row.get(i))==1&&Collections.frequency(col,col.get(i))==1){
+        //         count++;
+        //     }
+        // }
+        int row[]=new int[mat.length];
+        int col[]=new int[mat[0].length];
+        for(int i=0;i<row.length;i++){
+            for(int j=0;j<col.length;j++){
                 if(mat[i][j]==1){
-                    row.add(i);
-                    col.add(j);
+                    row[i]+=1;
+                    col[j]+=1;
                 }
             }
         }
-        // for(int i=0;i<mat.length;i++){
-        //     for(int j=0;j<mat[i].length;j++){
-        //         if(mat[i][j]==1&&Collections.frequency(row,i)==1&&Collections.frequency(col,j)==1){
-        //             count++;
-        //             break;
-        //         }
-
-        //     }
-        // }
-        for(int i=0;i<row.size();i++){
-            if(Collections.frequency(row,row.get(i))==1&&Collections.frequency(col,col.get(i))==1){
-                count++;
+        for(int i=0;i<row.length;i++){
+            for(int j=0;j<col.length;j++){
+                if(mat[i][j]==1&&row[i]==1&&col[j]==1){
+                    count++;
+                    break;
+                }
             }
         }
         return count;
