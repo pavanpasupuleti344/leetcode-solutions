47ce7049a4a83dabac9ea3b8ff98e0dca571b59f
Pavan Pasupuleti <pavanpasupuleti334@gmail.com>
2026-07-14 11:16:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (99.58%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -2,12 +2,14 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         o=[]
         for i in operations:
-            if i.isdigit() or (i.lstrip('-').isdigit()):
-                o.append(int(i))
-            elif i=='D':
+            # if i.isdigit() or (i.lstrip('-').isdigit()):
+            #     o.append(int(i))
+            if i=='D':
                 o.append(o[-1]*2)
             elif i=='C':
                 o.pop(-1)
             elif i=='+':
                 o.append(o[-1]+o[-2])
+            else:
+                o.append(int(i))
         return sum(o)
