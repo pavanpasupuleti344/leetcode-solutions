b44412b9a6cf03c5ca15c02aabee19925708d33e
Pavan Pasupuleti <pavanpasupuleti334@gmail.com>
2026-07-20 15:15:29 +0530

    Time: 0 ms (100%), Space: 19.6 MB (81.69%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -4,9 +4,11 @@ class Solution:
         l=[]
         for i in grid:
             l.extend(i)
-        for i in range(k):
-            l.insert(0,l[-1])
-            l.pop(-1)
-        print(l)
+        # for i in range(k):
+        #     l.insert(0,l[-1])
+        #     l.pop(-1)
+        l2=l[(len(l)-k):]
+        l2+=l[0:(len(l)-k)]
+        l=l2
         arr=[l[i:i+len(grid[0])] for i in range(0,len(l),len(grid[0]))]
         return arr
