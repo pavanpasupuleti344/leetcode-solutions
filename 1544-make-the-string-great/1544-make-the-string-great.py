9426e4ef10a716c5970bc5880bb2cc016f9fe284
Pavan Pasupuleti <pavanpasupuleti334@gmail.com>
2026-09-08 14:46:18 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.88%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,12 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        ans=[]
+        for i in s:
+            if len(ans)==0:
+                ans.append(i)
+                continue
+            if abs(ord(i)-ord(ans[-1]))==32:
+                ans.pop()
+            else:
+                ans.append(i)
+        return ''.join(ans)
