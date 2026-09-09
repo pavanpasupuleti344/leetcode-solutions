6c26d34e90176f117d8e851ed318d24b1b3538fe
Pavan Pasupuleti <pavanpasupuleti334@gmail.com>
2026-09-09 14:31:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.7%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -2,16 +2,16 @@ class Solution:
     def countCommas(self, n: int) -> int:
         num='1000'
         # digits=len(str(n))
-        def com(n):
-            digits=len(str(n))
-            comas=0
-            if digits%3==0:
-                comas=(digits//3)-1
-            else:
-                comas=digits//3
-            return comas
+        # def com(n):
+        #     digits=len(str(n))
+        #     comas=0
+        #     if digits%3==0:
+        #         comas=(digits//3)-1
+        #     else:
+        #         comas=digits//3
+        #     return comas
         
-        if com(n)==0:return 0
+        if n-int(num)<0:return 0
         # print('hi')
         ans=0
         while True:
