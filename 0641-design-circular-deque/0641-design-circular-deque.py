095aedeccc0ad6dfeeeb12b37595466dd2f2f3ae
Pavan Pasupuleti <pavanpasupuleti334@gmail.com>
2026-09-22 15:57:15 +0530

    Time: 7 ms (57.56%), Space: 20.1 MB (52.87%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,48 +1,55 @@
 class MyCircularDeque:
 
     def __init__(self, k: int):
-        self.d=[]
+        self.d=[None]*k
         self.k=k
         self.size=0
-        # self.front=self.rear=-1
+        self.front=self.rear=0
 
     def insertFront(self, value: int) -> bool:
         if self.size==self.k:
             return False
         self.size+=1
-        self.d.insert(0,value)
+        self.front=(self.front-1)%self.k
+        if self.front<0:
+            self.front+=len(self.d)
+        self.d[self.front]=value
         return True
 
     def insertLast(self, value: int) -> bool:
         if self.size==self.k:
             return False
-        self.d.append(value)
+        self.d[self.rear]=value
         self.size+=1
+        self.rear=(self.rear+1)%self.k
         return True
 
     def deleteFront(self) -> bool:
         if self.size==0:
             return False
         self.size-=1
-        self.d.pop(0)
+        self.front=(self.front+1)%self.k
         return True
 
     def deleteLast(self) -> bool:
         if self.size==0:
             return False
         self.size-=1
-        self.d.pop()
+        # self.front=(self.front-1)%self.k
+        self.rear=(self.rear-1)%self.k
+        if self.rear<0:
+            self.rear=len(self.d)-1
         return True
 
     def getFront(self) -> int:
         if self.size==0:
             return -1
-        return self.d[0]
+        return self.d[self.front]
 
     def getRear(self) -> int:
         if self.size==0:
             return -1
-        return self.d[-1]
+        return self.d[self.rear-1]
 
     def isEmpty(self) -> bool:
         return self.size==0
